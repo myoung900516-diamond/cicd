5becac4dfafdb2958da03e200dabb499aed32421
myoung900516-diamond <sophie0516@gmail.com>
2026-06-25 10:21:59 +0900

    CHANGE TEXT

--- deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
+++ deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@RequestMapping("/")
 	public String home() {
-		return "Welcome to server!";
+		return "HAVE A NICE DAY!";
 	}
 }
